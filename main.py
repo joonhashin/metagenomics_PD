ed9b9b16d0a30d37d2515e6c3cc0a187443a4700
jhlee4 <jhlee4@jlkgroup.com>
2021-12-02 16:48:27 +0900

    file name changes and merging function

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch
+
+from torch import nn
+from torch.functional import F
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(512, 1024)
+        self.linear2 = nn.Linear(1024, 1024)
+        self.linear3 = nn.Linear(1024, 32)
+        self.linear4 = nn.Linear(32, 1)
+    
+    def forward(self, input):
+        x = F.leaky_relu(self.linear1(input))
+        x = F.leaky_relu(self.linear2(x))
+        x = F.leaky_relu(self.linear3(x))
+        x = F.relu(self.linear4(x))
+        return x
+
+
--- preprocess.py
+++ preprocess.py
@@ -1,46 +1,83 @@
+import argparse
 import os
-import numpy as np
+import sys
+import re
 import pandas as pd
-from tqdm import tqdm
+from itertools import takewhile
 
-def preprocess(dir = 'PRJEB17784/', out_dir = 'profile_3/'):
-    # load labels
-    line_count = 0
-    id_list = []
-    label = {}
-    with open('sample_list.txt', 'r') as f:
-        for line in f:
-            line_data = line.rstrip()
-            line_count += 1
-            if line_count == 2:
-                id = line_data[24:-17]
-                id_list.append(id)
-            elif line_count == 14:
-                sample_label = line_data[19:-4]
-                label[id] = sample_label
-            elif line_count == 30:
-                line_count = 0
-    
-    df = pd.read_csv('filereport_read_run_PRJEB17784_tsv.txt', sep='\t')
-    for id in tqdm(id_list):
-        # load fastq file list
-        run_list = list(df[df['sample_accession']==id]['run_accession'])
+def merge( aaastrIn, ostm ):
+    """
+    Outputs the table join of the given pre-split string collection.
+    :param  aaastrIn:   One or more split lines from which data are read.
+    :type   aaastrIn:   collection of collections of string collections
+    :param  iCol:       Data column in which IDs are matched (zero-indexed).
+    :type   iCol:       int
+    :param  ostm:       Output stream to which matched rows are written.
+    :type   ostm:       output stream
+    """
 
-        # run metaphlan
-        string = ''
-        for run_id in run_list:
-            for file_name in os.listdir(os.path.join(dir,run_id)):
-                if os.path.splitext(file_name)[1] != '.gz':
-                    continue
-                file_path = os.path.join(dir, run_id,file_name)
-                string += file_path
-                string += ','
-        sample_label = label[id]
+    listmpaVersion = set()
+    merged_tables = pd.DataFrame()
 
-        bowtie = 'bowtie2out/'+id+'.bowtie2.bz2'
-        #os.system('metaphlan '+string[:-1]+ ' --bowtie2out bowtie2out/ '+ id+'.bowtie2.bz --input_type fastq > '+out_dir+id+'_'+sample_label+'_profile.txt')
-        os.system('metaphlan '+string[:-1]+ ' --bowtie2out '+bowtie+' --input_type fastq > '+out_dir+id+'_'+sample_label+'_profile.txt')
+    for f in aaastrIn:
+        with open(f) as fin:
+            headers = [x.strip() for x in takewhile(lambda x: x.startswith('#'), fin)]
+        if len(headers) == 1:
+            names = ['clade_name', 'relative_abundance']
+            index_col = 0
+        if len(headers) >= 4:
+            names = headers[-1].split('#')[1].strip().split('\t')
+            index_col = [0,1]
+
+        mpaVersion = list(filter(re.compile('#mpa_v[0-9]{2,}_CHOCOPhlAn\w*/{0,3}_[0-9]{0,}').match, headers))
+        if len(mpaVersion):
+            listmpaVersion.add(mpaVersion[0])
+        else:
+            print('merge_metaphlan_tables found tables without a header including the database version. Exiting.\n')
+            return
+        if len(listmpaVersion) > 1:
+            print('merge_metaphlan_tables found tables made with different versions of the MetaPhlAn database.\nPlease re-run MetaPhlAn with the same database.\n')
+            return
+        
+        iIn = pd.read_csv(f, 
+                          sep='\t',
+                          skiprows=len(headers),
+                          names = names,
+                          usecols=range(3),
+                        ).fillna('')
+        iIn = iIn.set_index(iIn.columns[index_col].to_list())
+        if merged_tables.empty:
+            merged_tables = iIn.iloc[:,0].rename(os.path.splitext(os.path.basename(f))[0]).to_frame()
+        else:
+            merged_tables = pd.merge(iIn.iloc[:,0].rename(os.path.splitext(os.path.basename(f))[0]).to_frame(),
+                                    merged_tables,
+                                    how='outer', 
+                                    left_index=True, 
+                                    right_index=True
+                                    )
+    if listmpaVersion:
+        ostm.write(list(listmpaVersion)[0]+'\n')
+    merged_tables.fillna('0').reset_index().to_csv(ostm, index=False, sep = '\t')
+
+argp = argparse.ArgumentParser( prog = "merge_metaphlan_tables.py",
+    description = """Performs a table join on one or more metaphlan output files.""")
+argp.add_argument( "aistms",    metavar = "input.txt", nargs = "+",
+    help = "One or more tab-delimited text tables to join" )
+argp.add_argument( '-o',    metavar = "output.txt", nargs = 1,
+    help = "Name of output file in which joined tables are saved" )
+
+__doc__ = "::\n\n\t" + argp.format_help( ).replace( "\n", "\n\t" )
+
+argp.usage = argp.format_usage()[7:]+"\n\n\tPlease make sure to supply file paths to the files to combine. If combining 3 files (Table1.txt, Table2.txt, and Table3.txt) the call should be:\n\n\t\tpython merge_metaphlan_tables.py Table1.txt Table2.txt Table3.txt > output.txt\n\n\tA wildcard to indicate all .txt files that start with Table can be used as follows:\n\n\t\tpython merge_metaphlan_tables.py Table*.txt > output.txt"
 
 
-if __name__ =="__main__":
-    preprocess()
+def main( ):
+    args = argp.parse_args( )
+    if args.o is None:
+        merge(args.aistms, sys.stdout)
+    else:
+        with open(args.o[0], 'w') as fout:
+            merge(args.aistms, fout)
+
+if __name__ == '__main__':
+    main()
