e0d1e7a42e225a1336112d9ebf5112c5d133fde6
jhshin <jooonhashin@gmail.com>
2021-12-14 15:01:40 +0900

    classification

--- main.py
+++ main.py
@@ -1,24 +1,64 @@
 import numpy as np
-import torch
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
-from torch import nn
-from torch.functional import F
+def main():
+    names = [
+        "Nearest Neighbors",
+        "Linear SVM",
+        "RBF SVM",
+        "Gaussian Process",
+        "Decision Tree",
+        "Random Forest",
+        "Neural Net",
+        "AdaBoost",
+        "Naive Bayes",
+        "QDA",
+    ]
+
+    classifiers = [
+        KNeighborsClassifier(3),
+        SVC(kernel="linear", C=0.025),
+        SVC(gamma=2, C=1),
+        GaussianProcessClassifier(1.0 * RBF(1.0)),
+        DecisionTreeClassifier(max_depth=5),
+        RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+        MLPClassifier(alpha=1, max_iter=1000),
+        AdaBoostClassifier(),
+        GaussianNB(),
+        QuadraticDiscriminantAnalysis(),
+    ]
+
+    with open('merged.txt') as f:
+        df = pd.read_csv(f,sep='\t',skiprows=1)
+
+    df = df.drop(['NCBI_tax_id', 'clade_name'], axis=1)
+    X = df.transpose().to_numpy().tolist()
+    y = [0]*28 + [1]*31
 
 
-class Model(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.linear1 = nn.Linear(512, 1024)
-        self.linear2 = nn.Linear(1024, 1024)
-        self.linear3 = nn.Linear(1024, 32)
-        self.linear4 = nn.Linear(32, 1)
-    
-    def forward(self, input):
-        x = F.leaky_relu(self.linear1(input))
-        x = F.leaky_relu(self.linear2(x))
-        x = F.leaky_relu(self.linear3(x))
-        x = F.relu(self.linear4(x))
-        return x
+    X = StandardScaler().fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.4, random_state=42
+    )
+
+
+    # iterate over classifiers
+    for name, clf in zip(names, classifiers):
+        clf.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+
+        print(name, score, flush=True)
 
 if __name__ == "__main__":
-    pass
+    main()
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,4 @@
-import argparse
 import os
-import sys
 import re
 import pandas as pd
 from itertools import takewhile
@@ -10,48 +8,59 @@ def preprocess():
     listmpaVersion = set()
     merged_tables = pd.DataFrame()
 
-    for f in os.listdir('profile/'):
-        if f[15:17]=="PD":
-            f = os.path.joi('profile/', f)
-            with open(f) as fin:
-                headers = [x.strip() for x in takewhile(lambda x: x.startswith('#'), fin)]
-            if len(headers) == 1:
-                names = ['clade_name', 'relative_abundance']
-                index_col = 0
-            if len(headers) >= 4:
-                names = headers[-1].split('#')[1].strip().split('\t')
-                index_col = [0,1]
+    pd_list = []
+    co_list = []
 
-            mpaVersion = list(filter(re.compile('#mpa_v[0-9]{2,}_CHOCOPhlAn\w*/{0,3}_[0-9]{0,}').match, headers))
-            if len(mpaVersion):
-                listmpaVersion.add(mpaVersion[0])
-            else:
-                print('merge_metaphlan_tables found tables without a header including the database version. Exiting.\n')
-                return
-            if len(listmpaVersion) > 1:
-                print('merge_metaphlan_tables found tables made with different versions of the MetaPhlAn database.\nPlease re-run MetaPhlAn with the same database.\n')
-                return
-            
-            iIn = pd.read_csv(f, 
-                            sep='\t',
-                            skiprows=len(headers),
-                            names = names,
-                            usecols=range(3),
-                            ).fillna('')
-            iIn = iIn.set_index(iIn.columns[index_col].to_list())
-            if merged_tables.empty:
-                merged_tables = iIn.iloc[:,0].rename(os.path.splitext(os.path.basename(f))[0]).to_frame()
-            else:
-                merged_tables = pd.merge(iIn.iloc[:,0].rename(os.path.splitext(os.path.basename(f))[0]).to_frame(),
-                                        merged_tables,
-                                        how='outer', 
-                                        left_index=True, 
-                                        right_index=True
-                                        )
-        with open('pd_merged.txt', 'w') as fout:
-            if listmpaVersion:
-                fout.write(list(listmpaVersion)[0]+'\n')
-            merged_tables.fillna('0').reset_index().to_csv(fout, index=False, sep = '\t')
+    for file in os.listdir('profile/'):
+        if file[15:17]=="PD":
+            pd_list.append(os.path.join('profile/', file))
+        else:
+            co_list.append(os.path.join('profile/', file))
+    print(len(pd_list))
+    print(len(co_list))
+    
+    ordered_list = pd_list + co_list
+
+    for f in ordered_list:
+        with open(f) as fin:
+            headers = [x.strip() for x in takewhile(lambda x: x.startswith('#'), fin)]
+        if len(headers) == 1:
+            names = ['clade_name', 'relative_abundance']
+            index_col = 0
+        if len(headers) >= 4:
+            names = headers[-1].split('#')[1].strip().split('\t')
+            index_col = [0,1]
+
+        mpaVersion = list(filter(re.compile('#mpa_v[0-9]{2,}_CHOCOPhlAn\w*/{0,3}_[0-9]{0,}').match, headers))
+        if len(mpaVersion):
+            listmpaVersion.add(mpaVersion[0])
+        else:
+            print('merge_metaphlan_tables found tables without a header including the database version. Exiting.\n')
+            return
+        if len(listmpaVersion) > 1:
+            print('merge_metaphlan_tables found tables made with different versions of the MetaPhlAn database.\nPlease re-run MetaPhlAn with the same database.\n')
+            return
+        
+        iIn = pd.read_csv(f, 
+                        sep='\t',
+                        skiprows=len(headers),
+                        names = names,
+                        usecols=range(3),
+                        ).fillna('')
+        iIn = iIn.set_index(iIn.columns[index_col].to_list())
+        if merged_tables.empty:
+            merged_tables = iIn.iloc[:,0].rename(os.path.splitext(os.path.basename(f))[0]).to_frame()
+        else:
+            merged_tables = pd.merge(iIn.iloc[:,0].rename(os.path.splitext(os.path.basename(f))[0]).to_frame(),
+                                    merged_tables,
+                                    how='outer', 
+                                    left_index=True, 
+                                    right_index=True
+                                    )
+    with open('merged.txt', 'w') as fout:
+        if listmpaVersion:
+            fout.write(list(listmpaVersion)[0]+'\n')
+        merged_tables.fillna('0').reset_index().to_csv(fout, index=False, sep = '\t')
 
 if __name__ == '__main__':
     preprocess()
