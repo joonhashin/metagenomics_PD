311a865c671820aaf903d2a9aedd0f12bd568e87
jhshin <jooonhashin@gmail.com>
2021-12-15 10:09:00 +0900

    deleted load_data

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import load_data, preprocess, classify
+import data.tutorial.load_data as load_data, preprocess, classify
+
+
+
 
 if __name__ =="__main__":
-    load_data()
+
+    input = {
+        "filereport" : 'filereport_read_run_PRJEB17784_tsv.txt', 
+        "sample_list" : 'sample_list.txt', 
+        "dir" : 'data/tutorial/', 
+        "out_dir" : 'profile/'
+    }
+
+    load_data(**input)
     preprocess()
     classify()
-    
